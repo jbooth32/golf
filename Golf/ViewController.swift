0ae51a1258549ee21976ba0d174debb7c499044c
Jared <boothjar@grinnell.edu>
2020-05-02 15:22:26 -0500

    Initial Commit

---
+++ Golf/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Golf
+//
+//  Created by Jared on 5/2/20.
+//  Copyright © 2020 Jared. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
